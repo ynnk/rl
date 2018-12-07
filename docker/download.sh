015caec4be1676166a1f09452b9de03118e223cf
root <root@int.atilf.fr>
2018-12-07 16:11:28 +0100

    modify dockerfiles

--- docker/download.sh
+++ docker/download.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-SOURCE="spiderlexdb.tgz"
+SOURCE="spiderlex.tgz"
+PARSER=/usr/local/rl/parser
+TARGET=/usr/local/rl
 
 cd /tmp
 
@@ -11,6 +13,15 @@ fi
 
 echo "downlading spiderlex.tgz"
 wget -q http://lexsys.atilf.fr/export/$SOURCE
-cd /usr/local/rl
+
 echo "unpacking files..."
-tar zxf /tmp/$SOURCE
+tar zxf $SOURCE
+
+echo "converting graphs"
+python3 $PARSER/parse.py lnfr ./ls-fr-spiderlex/ --complete $TARGET/completedb_fr.sqlite -s igraph -o $TARGET/lnfr.picklez
+python3 $PARSER/parse.py lnen ./ls-en-spiderlex/ --complete $TARGET/completedb_en.sqlite -s igraph -o $TARGET/lnen.picklez
+
+echo "removing temp"
+rm -rf ./ls-fr-spiderlex ./ls-en-spiderlex $SOURCE
+
+echo "done"
